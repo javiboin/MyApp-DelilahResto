52705faf86428b9d43c1bef649625f3f698801d1
Javier Alejandro Oyarzo Carcamo <javi_14_228@hotmail.com>
2021-08-15 06:02:25 -0300

    Swagger Conf in progress

--- src/backend/server.js
+++ src/backend/server.js
@@ -50,10 +50,6 @@ router.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 /* ---------------- ENDPOINTS ---------------- */
 /* ------------------------------------------- */
 
-router.get('/filterUsers', function (req, res){
-  res.send(functions.filterUsers(1));
-});
-
 const login = (username, password) => users.find(user => user.username === username && user.password === password);
 
 router.post('/login', (req, res) => {
@@ -65,18 +61,126 @@ router.post('/login', (req, res) => {
 });
 
 /* ---------------- USUARIOS ---------------- */
+router.get('/filterUsers', function (req, res){
+  res.send(functions.filterUsers(1));
+});
 
+/**
+ * @swagger
+ * /users:
+ *  get:
+ *    description: Listado de todos los usuarios
+ *    responses:
+ *      200:
+ *        description: Success
+ */
 router.get('/users', function (req, res){
   let respuesta = {};
   respuesta.msg = functions.listUsers();
   res.json(respuesta);
 });
 
+/**
+ * @swagger
+ * /users:
+ *  post:
+ *    description: Crea un Usuario
+ *    parameters:
+ *    - name: id
+ *      description: Id de Usuario
+ *      in: formData
+ *      required: false
+ *      type: integer
+ *    - name: nickname
+ *      description: Nombre de Usuario 
+ *      in: formData
+ *      required: true
+ *      type: string
+ *    - name: completeName
+ *      description: Nombre del propietario de la cuenta 
+ *      in: formData
+ *      required: true
+ *      type: string
+ *    - name: email
+ *      description: Correo electronico de Usuario 
+ *      in: formData
+ *      required: true
+ *      type: string
+ *    - name: phone
+ *      description: Numero de telefono de Usuario 
+ *      in: formData
+ *      required: true
+ *      type: integer
+ *    - name: mainAddress
+ *      description: Domicilio de Usuario 
+ *      in: formData
+ *      required: true
+ *      type: string
+ *    - name: altAddress
+ *      description: Domicilio alternativo o transitorio 
+ *      in: formData
+ *      required: true
+ *      type: string
+ *    - name: password
+ *      description: Contraseña de Usuario 
+ *      in: formData
+ *      required: true
+ *      type: string
+ */
+
 router.post('/users', function (req, res){
   let respuesta = {};
   respuesta.msg = functions.crearUser(req.body);
   res.json(respuesta);
 });
+
+/**
+ * @swagger
+ * /users/{id}:
+ *  put:
+ *    description: Modifica un Usuario
+ *    parameters:
+ *    - name: id
+ *      description: Id de Usuario
+ *      in: formData
+ *      required: true
+ *      type: integer
+ *    - name: nickname
+ *      description: Nombre de Usuario 
+ *      in: formData
+ *      required: true
+ *      type: string
+ *    - name: completeName
+ *      description: Nombre del propietario de la cuenta 
+ *      in: formData
+ *      required: true
+ *      type: string
+ *    - name: email
+ *      description: Correo electronico de Usuario 
+ *      in: formData
+ *      required: true
+ *      type: string
+ *    - name: phone
+ *      description: Numero de telefono de Usuario 
+ *      in: formData
+ *      required: true
+ *      type: integer
+ *    - name: mainAddress
+ *      description: Domicilio de Usuario 
+ *      in: formData
+ *      required: true
+ *      type: string
+ *    - name: altAddress
+ *      description: Domicilio alternativo o transitorio 
+ *      in: formData
+ *      required: true
+ *      type: string
+ *    - name: password
+ *      description: Contraseña de Usuario 
+ *      in: formData
+ *      required: true
+ *      type: string
+ */
 
 router.put('/users/:id', function (req, res){
   const idUser = req.params.id;
@@ -101,13 +205,53 @@ router.delete('/users/:id', function (req, res){
 });
 
 /* ---------------- PEDIDOS ---------------- */
-
+/**
+ * @swagger
+ * /orders:
+ *  get:
+ *    description: Listado de todos los pedidos
+ *    responses:
+ *      200:
+ *        description: Success
+ */
 router.get('/orders', function (req, res){
   let respuesta = {};
   respuesta.msg = functions.listOrders();
   res.json(respuesta);
 });
 
+/**
+ * @swagger
+ * /users:
+ *  post:
+ *    description: Crea un Usuario
+ *    parameters:
+ *    - name: id
+ *      description: Id de Usuario
+ *      in: formData
+ *      required: false
+ *      type: integer
+ *    - name: nickname
+ *      description: Nombre de Usuario 
+ *      in: formData
+ *      required: true
+ *      type: string
+ *    - name: completeName
+ *      description: Nombre del propietario de la cuenta 
+ *      in: formData
+ *      required: true
+ *      type: string
+ *    - name: email
+ *      description: Correo electronico de Usuario 
+ *      in: formData
+ *      required: true
+ *      type: string
+ *    - name: phone
+ *      description: Numero de telefono de Usuario 
+ *      in: formData
+ *      required: true
+ *      type: integer
+ */
 router.post('/orders', function (req, res){
   let respuesta = {};
   respuesta.msg = functions.crearOrder(req.body);
