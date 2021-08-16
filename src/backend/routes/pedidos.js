ae41e97137f41c34f1dd52353b535badf558781c
Javier Alejandro Oyarzo Carcamo <javi_14_228@hotmail.com>
2021-08-15 21:16:32 -0300

    Add routes folder

---
+++ src/backend/routes/pedidos.js
@@ -0,0 +1,174 @@
+
+
+/**
+ * @swagger
+ * /orders:
+ *  get:
+ *    description: Listado de todos los pedidos
+ *    responses:
+ *      200:
+ *        description: Success
+ */
+router.get('/orders', function (req, res){
+  let respuesta = {};
+  respuesta.msg = functions.listOrders();
+  res.json(respuesta);
+});
+
+/**
+ * @swagger
+ * /orders:
+ *  post:
+ *    description: Crea un pedido
+ *    parameters:
+ *    - name: id
+ *      description: Id de pedido
+ *      in: formData
+ *      required: false
+ *      type: integer
+ *    - name: state
+ *      description: Estado del pedido 
+ *      in: formData
+ *      required: true
+ *      type: string
+ *    - name: products
+ *      description: Listado de productos en el pedido 
+ *      in: formData
+ *      required: true
+ *      type: array
+ *    - name: formaPago
+ *      description: Metodos de pago 
+ *      in: formData
+ *      required: true
+ *      type: string
+ *    - name: price
+ *      description: Precio del pedido 
+ *      in: formData
+ *      required: true
+ *      type: integer
+ */
+router.post('/orders', function (req, res){
+  let respuesta = {};
+  respuesta.msg = functions.crearOrder(req.body);
+  res.json(respuesta);
+});
+
+/**
+ * @swagger
+ * /orders:
+ *  put:
+ *    description: Crea un pedido
+ *    parameters:
+ *    - name: id
+ *      description: Id de pedido
+ *      in: formData
+ *      required: false
+ *      type: integer
+ *    - name: state
+ *      description: Estado del pedido 
+ *      in: formData
+ *      required: true
+ *      type: string
+ *    - name: products
+ *      description: Listado de productos en el pedido 
+ *      in: formData
+ *      required: true
+ *      type: array
+ *    - name: formaPago
+ *      description: Metodos de pago 
+ *      in: formData
+ *      required: true
+ *      type: string
+ *    - name: price
+ *      description: Precio del pedido 
+ *      in: formData
+ *      required: true
+ *      type: integer
+ */
+
+router.put('/orders/:id', function (req, res){
+  const idOrders = req.params.id;
+  let respuesta = {};
+  respuesta.msg = functions.filterOrders(idOrders) ? functions.modificarOrder(idOrders ,req.body) : "no es correcto";
+  res.json(respuesta);
+});
+
+/**
+ * @swagger
+ * /orders/{id}:
+ *  get:
+ *    description: Crea un pedido
+ *    parameters:
+ *    - name: id
+ *      description: Id de pedido
+ *      in: formData
+ *      required: false
+ *      type: integer
+ *    - name: state
+ *      description: Estado del pedido 
+ *      in: formData
+ *      required: true
+ *      type: string
+ *    - name: products
+ *      description: Listado de productos en el pedido 
+ *      in: formData
+ *      required: true
+ *      type: array
+ *    - name: formaPago
+ *      description: Metodos de pago 
+ *      in: formData
+ *      required: true
+ *      type: string
+ *    - name: price
+ *      description: Precio del pedido 
+ *      in: formData
+ *      required: true
+ *      type: integer
+ */
+
+router.get('/orders/:id', function (req, res){
+  const idOrders = req.params.id;
+  let respuesta = {};
+  respuesta.msg = functions.filterOrders ? functions.orderID(idOrders) : "no es correcto";
+  res.json(respuesta);
+});
+
+/**
+ * @swagger
+ * /orders/{id}:
+ *  delete:
+ *    description: Crea un pedido
+ *    parameters:
+ *    - name: id
+ *      description: Id de pedido
+ *      in: formData
+ *      required: false
+ *      type: integer
+ *    - name: state
+ *      description: Estado del pedido 
+ *      in: formData
+ *      required: true
+ *      type: string
+ *    - name: products
+ *      description: Listado de productos en el pedido 
+ *      in: formData
+ *      required: true
+ *      type: array
+ *    - name: formaPago
+ *      description: Metodos de pago 
+ *      in: formData
+ *      required: true
+ *      type: string
+ *    - name: price
+ *      description: Precio del pedido 
+ *      in: formData
+ *      required: true
+ *      type: integer
+ */
+
+router.delete('/orders/:id', function (req, res){
+  const idOrders = req.params.id;
+  let respuesta = {};
+  respuesta.msg = functions.filterOrders ? functions.borrarOrder(idOrders) : "no es correcto";
+  res.json(respuesta);
+});
---
+++ src/backend/routes/usuarios.js
@@ -0,0 +1,240 @@
+
+
+router.get('/filterUsers', function (req, res){
+  res.send(functions.filterUsers(1));
+});
+
+/**
+ * @swagger
+ * /users:
+ *  get:
+ *    description: Listado de todos los usuarios
+ *    responses:
+ *      200:
+ *        description: Success
+ */
+router.get('/users', function (req, res){
+  let respuesta = {};
+  respuesta.msg = functions.listUsers();
+  res.json(respuesta);
+});
+/**
+ * @swagger
+ * /users:
+ *  post:
+ *    description: Crea un Usuario
+ *    parameters:
+ *    - name: id
+ *      description: Id de Usuario
+ *      in: formData
+ *      required: false
+ *      type: integer
+ *    - name: nickname
+ *      description: Nombre de Usuario 
+ *      in: formData
+ *      required: true
+ *      type: string
+ *    - name: completeName
+ *      description: Nombre del propietario de la cuenta 
+ *      in: formData
+ *      required: true
+ *      type: string
+ *    - name: email
+ *      description: Correo electronico de Usuario 
+ *      in: formData
+ *      required: true
+ *      type: string
+ *    - name: phone
+ *      description: Numero de telefono de Usuario 
+ *      in: formData
+ *      required: true
+ *      type: integer
+ *    - name: mainAddress
+ *      description: Domicilio de Usuario 
+ *      in: formData
+ *      required: true
+ *      type: string
+ *    - name: altAddress
+ *      description: Domicilio alternativo o transitorio 
+ *      in: formData
+ *      required: true
+ *      type: string
+ *    - name: password
+ *      description: Contraseña de Usuario 
+ *      in: formData
+ *      required: true
+ *      type: string
+ */
+
+router.post('/users', function (req, res){
+  let respuesta = {};
+  respuesta.msg = functions.crearUser(req.body);
+  res.json(respuesta);
+  console.log(req.body);
+  res.status(201).send({error: '', body: 'Creado Correctamente'});
+});
+
+/**
+ * @swagger
+ * /users/{id}:
+ *  put:
+ *    description: Modifica un Usuario
+ *    parameters:
+ *    - name: id
+ *      description: Id de Usuario
+ *      in: formData
+ *      required: true
+ *      type: integer
+ *    - name: nickname
+ *      description: Nombre de Usuario 
+ *      in: formData
+ *      required: true
+ *      type: string
+ *    - name: completeName
+ *      description: Nombre del propietario de la cuenta 
+ *      in: formData
+ *      required: true
+ *      type: string
+ *    - name: email
+ *      description: Correo electronico de Usuario 
+ *      in: formData
+ *      required: true
+ *      type: string
+ *    - name: phone
+ *      description: Numero de telefono de Usuario 
+ *      in: formData
+ *      required: true
+ *      type: integer
+ *    - name: mainAddress
+ *      description: Domicilio de Usuario 
+ *      in: formData
+ *      required: true
+ *      type: string
+ *    - name: altAddress
+ *      description: Domicilio alternativo o transitorio 
+ *      in: formData
+ *      required: true
+ *      type: string
+ *    - name: password
+ *      description: Contraseña de Usuario 
+ *      in: formData
+ *      required: true
+ *      type: string
+ */
+
+ router.put('/users/:id', function (req, res){
+  const idUser = req.params.id;
+  let respuesta = {};
+  respuesta.msg = functions.filterUsers(idUser) ? 
+  functions.modificarUser(idUser,req.body) : `ese autor no existe, puede ver todos los autores en ${url}/autores`;
+  res.json(respuesta);
+});
+
+/**
+ * @swagger
+ * /users/{id}:
+ *  get:
+ *    description: Modifica un Usuario
+ *    parameters:
+ *    - name: id
+ *      description: Id de Usuario
+ *      in: formData
+ *      required: true
+ *      type: integer
+ *    - name: nickname
+ *      description: Nombre de Usuario 
+ *      in: formData
+ *      required: true
+ *      type: string
+ *    - name: completeName
+ *      description: Nombre del propietario de la cuenta 
+ *      in: formData
+ *      required: true
+ *      type: string
+ *    - name: email
+ *      description: Correo electronico de Usuario 
+ *      in: formData
+ *      required: true
+ *      type: string
+ *    - name: phone
+ *      description: Numero de telefono de Usuario 
+ *      in: formData
+ *      required: true
+ *      type: integer
+ *    - name: mainAddress
+ *      description: Domicilio de Usuario 
+ *      in: formData
+ *      required: true
+ *      type: string
+ *    - name: altAddress
+ *      description: Domicilio alternativo o transitorio 
+ *      in: formData
+ *      required: true
+ *      type: string
+ *    - name: password
+ *      description: Contraseña de Usuario 
+ *      in: formData
+ *      required: true
+ *      type: string
+ */
+
+router.get('/users/:id', function (req, res){
+  const idUser = req.params.id;
+  let respuesta = {};
+  respuesta.msg = functions.filterUsers(idUser) ? functions.userID(idUser) : "no es correcto";
+  res.json(respuesta); 
+});
+
+/**
+ * @swagger
+ * /users/{id}:
+ *  delete:
+ *    description: Modifica un Usuario
+ *    parameters:
+ *    - name: id
+ *      description: Id de Usuario
+ *      in: formData
+ *      required: true
+ *      type: integer
+ *    - name: nickname
+ *      description: Nombre de Usuario 
+ *      in: formData
+ *      required: true
+ *      type: string
+ *    - name: completeName
+ *      description: Nombre del propietario de la cuenta 
+ *      in: formData
+ *      required: true
+ *      type: string
+ *    - name: email
+ *      description: Correo electronico de Usuario 
+ *      in: formData
+ *      required: true
+ *      type: string
+ *    - name: phone
+ *      description: Numero de telefono de Usuario 
+ *      in: formData
+ *      required: true
+ *      type: integer
+ *    - name: mainAddress
+ *      description: Domicilio de Usuario 
+ *      in: formData
+ *      required: true
+ *      type: string
+ *    - name: altAddress
+ *      description: Domicilio alternativo o transitorio 
+ *      in: formData
+ *      required: true
+ *      type: string
+ *    - name: password
+ *      description: Contraseña de Usuario 
+ *      in: formData
+ *      required: true
+ *      type: string
+ */
+router.delete('/users/:id', function (req, res){
+  const idUser = req.params.id;
+  let respuesta = {};
+  respuesta.msg = functions.filterUsers(idUser) ? functions.borrarUser(idUser) : "no es correcto";
+  res.json(respuesta);
+});
