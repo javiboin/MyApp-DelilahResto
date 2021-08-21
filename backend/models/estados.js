32da5666ea7f7da21a2a0aa353c0ef7fcfc93358
Javier Alejandro Oyarzo Carcamo <javi_14_228@hotmail.com>
2021-08-21 17:29:43 -0300

    Update estados.js

--- backend/models/estados.js
+++ backend/models/estados.js
@@ -1,10 +1,10 @@
 let estadosDePedidos = [
-  "Nuevo",
-  "Confirmado",
-  "Preparando",
-  "Enviando",
-  "Cancelado",
-  "Entregado"
+  {id: 1, name: "Nuevo"},
+  {id: 2, name: "Confirmado"},
+  {id: 3, name: "Preparando"},
+  {id: 4, name: "Enviando"},
+  {id: 5, name: "Cancelado"},
+  {id: 6, name: "Entregado"}
 ];
 
 module.exports = estadosDePedidos;
