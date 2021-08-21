7c09d17e7008e160af18ce11424978abaf2b15c0
Javier Alejandro Oyarzo Carcamo <javi_14_228@hotmail.com>
2021-08-21 19:05:03 -0300

    Revision administradores.js OK

--- backend/controllers/administradores.js
+++ backend/controllers/administradores.js
@@ -2,6 +2,6 @@ const administradores = require('../models/administradores');
 
 function isAdmin(id) {
   return administradores.find(administrador => administrador === id) ? true : false;
-}
+};
 
 exports.isAdmin = isAdmin;
