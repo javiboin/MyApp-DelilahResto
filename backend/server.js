af9435705e8f793e29f7d7c549c7dad3b133d9aa
Javier Alejandro Oyarzo Carcamo <javi_14_228@hotmail.com>
2021-08-29 19:04:19 -0300

    Change Versions 1.0

--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,7 @@ const swaggerOptions = {
   swaggerDefinition: {
     info: {
       title: 'Delilah Restó API',
-      version: '0.9.9'
+      version: '1.0.0'
     }
   },
   apis: ['./routes/usuarios.js', './routes/pedidos.js', './routes/productos.js','./routes/metodosPago.js']
