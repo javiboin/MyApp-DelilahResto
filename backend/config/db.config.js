e89511b38cd4395bc8a90d1991de14323ff83d47
Javier Alejandro Oyarzo Carcamo <javi_14_228@hotmail.com>
2021-09-29 21:35:40 -0300

    Connection DB OK

---
+++ backend/config/db.config.js
@@ -0,0 +1,17 @@
+require('dotenv').config()
+const Sequelize = require('sequelize');
+
+const connection = new Sequelize(
+  process.env.MYSQL_DELILAH_DB_NAME, 
+  process.env.DB_USER, 
+  process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'mysql',
+    define: {
+      freezeTableName: true
+    }
+  });
+
+
+  module.exports = connection;
--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,16 @@
-const app = require('./server');
 
-const PORT = 3001;
+require('dotenv').config();
+const app = require('./server');
+const connection = require("./config/db.config");
+const PORT = process.env.PORT || 3030;
 const host = 'http://localhost'
 const url = `${host}:${PORT}`;
 
-app.listen(PORT, () => console.log(`Listen on ${url}`));
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+  connection.authenticate().then(() => {
+      console.log('BBDD conectada');
+  }).catch((err) => {
+      console.log('BBDD error', err);
+  })
+});
