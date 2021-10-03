8af53f3f20dff98752f64f927a7567c83a923bbd
Javier Alejandro Oyarzo Carcamo <javi_14_228@hotmail.com>
2021-10-02 23:16:05 -0300

    Add Model User

---
+++ backend/models/user.model.js
@@ -0,0 +1,32 @@
+const UserModel = (connection, Sequelize) => {
+  const User = connection.define('users', {
+    id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true
+    },
+    nickname: {
+      type: Sequelize.STRING
+    },
+    completename: {
+      type: Sequelize.STRING
+    },
+    email: {
+      type: Sequelize.STRING
+    },
+    phone: {
+      type: Sequelize.STRING
+    },
+    address: {
+      type: Sequelize.STRING
+    },
+    password: {
+      type: Sequelize.STRING
+    }
+  },
+  {
+    timestamps: false
+  });
+  return User
+};
+
+module.exports = UserModel;
