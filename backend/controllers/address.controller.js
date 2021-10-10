1bd4fc2cabfd33f1a98efac9a6a7c8cc986dd81d
Javier Alejandro Oyarzo Carcamo <javi_14_228@hotmail.com>
2021-10-10 20:48:43 -0300

    Add POST Address

--- backend/controllers/address.controller.js
+++ backend/controllers/address.controller.js
@@ -5,6 +5,17 @@ const AddressModel = require('../models/address.model')(connection, Sequelize);
 
 const listValues = async () => await AddressModel.findAll();
 
+const   createAddress = async (req) => {
+  const newAddress = await AddressModel.build({
+    name: req.body.name,
+    number: req.body.number,
+  });
+
+  const result = await newAddress.save();
+  return result;
+}
+
 module.exports = {
-  listValues
+  listValues,
+  createAddress
 };
