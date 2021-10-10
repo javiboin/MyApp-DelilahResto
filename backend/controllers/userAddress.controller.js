1f698b0fec9f770a889d1a698a749bcbd7a32f71
Javier Alejandro Oyarzo Carcamo <javi_14_228@hotmail.com>
2021-10-10 20:43:32 -0300

    Add POST User Address

--- backend/controllers/userAddress.controller.js
+++ backend/controllers/userAddress.controller.js
@@ -5,6 +5,17 @@ const UserAddressModel = require('../models/userAddress.model')(connection, Sequ
 
 const listValues = async () => await UserAddressModel.findAll();
 
+const createUserAddress = async (req) => {
+  const newUserAddress = await UserAddressModel.build({
+    id_user: req.body.id_user,
+    id_address: req.body.id_address,
+  });
+
+  const result = await newUserAddress.save();
+  return result;
+}
+
 module.exports = {
-  listValues
+  listValues,
+  createUserAddress
 };
