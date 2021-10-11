26bc130d4d819651961ec2dbd35789dbff36c125
Javier Alejandro Oyarzo Carcamo <javi_14_228@hotmail.com>
2021-10-10 22:04:52 -0300

    Add POST Products

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -5,6 +5,18 @@ const ProductModel = require('../models/product.model')(connection, Sequelize);
 
 const listValues = async () => await ProductModel.findAll();
 
+const createProduct = async (req) => {
+  const newProduct = await ProductModel.build({
+    name: req.body.name,
+    price: req.body.id_user,
+    pic: req.body.pic
+  });
+
+  const result = await newProduct.save();
+  return result;
+}
+
 module.exports = {
-  listValues
+  listValues,
+  createProduct
 };
--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -26,6 +26,22 @@ router.get("/", (req, res) => {
   });
 });
 
+router.post("/", (req, res) => {
+  productController.createProduct(req)
+  .then(() => {
+    res.status(200).send({
+      status: 200,
+      message: "Data Save Successfully",
+    });
+  })
+  .catch(error => {
+    res.status(400).send({
+      message: "Unable to insert data",
+      errors: error,
+      status: 400
+    });
+  });
+});
 
 /**
  * @swagger
