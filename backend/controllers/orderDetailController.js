564cd1dc45c0c44b507b6c6f7b57016a6b796a24
Javier Alejandro Oyarzo Carcamo <javi_14_228@hotmail.com>
2021-10-11 04:28:01 -0300

    Add GET by id & PUT & DELETE Order Details

--- backend/controllers/orderDetailController.js
+++ backend/controllers/orderDetailController.js
@@ -17,7 +17,37 @@ const createOrderDetail = async (req) => {
   return result;
 }
 
+const updateOrderDetail = async (req) => {
+  const id_order_detail = parseInt(req.params.id);
+  const result = await orderDetailModel.update({
+    id_order: req.body.id_order,
+    id_product: req.body.id_product,
+    quantity: req.body.quantity,
+    price: req.body.price
+    },
+    { where: { id: id_order_detail } }
+  );
+  return result;
+}
+
+const deleteOrderDetail = async (req) => {
+  const id_order_detail = parseInt(req.params.id);
+  const result = await orderDetailModel.destroy({
+    where: { id: id_order_detail }
+  });
+  return result;
+}
+
+const listOrderDetailById = async (req) => {
+  const id_order_detail = parseInt(req.params.id);
+  const result = await orderDetailModel.findOne({ where: { id: id_order_detail } });
+  return result;
+}
+
 module.exports = {
   listValues,
-  createOrderDetail
+  createOrderDetail,
+  updateOrderDetail,
+  deleteOrderDetail,
+  listOrderDetailById
 };
