cb5d7e925789deadaec93571c72012e2bf3dd58d
Javier Alejandro Oyarzo Carcamo <javi_14_228@hotmail.com>
2021-10-11 01:13:44 -0300

    Add GET by id & UPDATE & DELETE User Suspensions

--- backend/controllers/userSuspension.controller.js
+++ backend/controllers/userSuspension.controller.js
@@ -8,14 +8,43 @@ const listValues = async () => await UserSuspensionModel.findAll();
 const createUserSuspension = async (req) => {
   const newUserSuspension = await UserSuspensionModel.build({
     reason: req.body.reason,
-    id_user: req.body.id_user,
+    id_user: req.body.id_user
   });
 
   const result = await newUserSuspension.save();
+  return result;
+};
+
+const updateUserSupension = async (req) => {
+  const id_user_suspension = parseInt(req.params.id);
+  const result = await UserSuspensionModel.update({
+    reason: req.body.reason,
+    id_user: req.body.id_user
+    },
+    { where: { id: id_user_suspension } }
+  );
+  return result;
+}
+
+const deleteUserSupension = async (req) => {
+  const id_user_suspension = parseInt(req.params.id);
+  const result = await UserSuspensionModel.destroy({
+    where: { id: id_user_suspension }
+  });
+  return result;
+}
+
+const listUserSupensionById = async (req) => {
+  const id_user_suspension = parseInt(req.params.id);
+  console.log(id_user_suspension);
+  const result = await UserSuspensionModel.findOne({ where: { id: id_user_suspension } });
   return result;
 }
 
 module.exports = {
   listValues,
-  createUserSuspension
+  createUserSuspension,
+  updateUserSupension,
+  deleteUserSupension,
+  listUserSupensionById
 };
--- backend/routes/userSuspension.route.js
+++ backend/routes/userSuspension.route.js
@@ -38,6 +38,58 @@ router.post("/", (req, res) => {
   });
 });
 
+router.put("/:id",(req, res) => {
+  userSuspensionController.updateUserSupension(req)
+  .then(() => {
+    res.status(200).send({
+      status: 200,
+      message: "Data Update Successfully",
+    });
+  })
+  .catch(error => {
+    res.status(400).send({
+      message: "Unable to Update data",
+      errors: error,
+      status: 400
+    });
+  });
+});
+
+router.delete("/:id", (req, res) => {
+  userSuspensionController.deleteUserSupension(req)
+  .then(() => {
+    res.status(200).send({
+      status: 200,
+      message: "Data Delete Successfully",
+    });
+  })
+  .catch(error => {
+    res.status(400).send({
+      message: "Unable to Delete data",
+      errors: error,
+      status: 400
+    });
+  });
+});
+
+router.get("/:id", (req, res) => {
+  userSuspensionController.listUserSupensionById(req)
+  .then((result) => {
+    res.status(200).send({
+      status: 200,
+      message: "Data find Successfully",
+      data: result
+    });
+  })
+  .catch(error => {
+    res.status(400).send({
+      message: "Unable to find data",
+      errors: error,
+      status: 400
+    });
+  });
+});
+
 /* router.get("/:id", (req, res) => {
   userSuspensionController.ValueById()
   .then((result) => {
