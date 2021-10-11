5c2c777f319e0bd9bb44a84a31814acb6100159f
Javier Alejandro Oyarzo Carcamo <javi_14_228@hotmail.com>
2021-10-10 22:17:36 -0300

    Add POST Orders Detail

--- backend/controllers/orderDetailController.js
+++ backend/controllers/orderDetailController.js
@@ -5,6 +5,19 @@ const orderDetailModel = require('../models/orderDetail.model')(connection, Sequ
 
 const listValues = async () => await orderDetailModel.findAll();
 
+const createOrderDetail = async (req) => {
+  const newOrderDetail = await orderDetailModel.build({
+    id_order: req.body.id_order,
+    id_product: req.body.id_product,
+    quantity: req.body.quantity,
+    price: req.body.price,
+  });
+
+  const result = await newOrderDetail.save();
+  return result;
+}
+
 module.exports = {
-  listValues
+  listValues,
+  createOrderDetail
 };
