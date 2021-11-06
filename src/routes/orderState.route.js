3d9b8d61050072acb9c48def679e8383431cbc0d
Javier Alejandro Oyarzo Carcamo <javi_14_228@hotmail.com>
2021-11-06 18:00:32 -0300

    ADD CRRD operations in Swagger Order States

--- src/routes/orderState.route.js
+++ src/routes/orderState.route.js
@@ -91,4 +91,147 @@ router.get("/:id", all.isAdmin, (req, res) => {
   });
 });
 
+/**
+ * @swagger
+ * /order-states:
+ *  get:
+ *    tags:
+ *    - "Estados de los Pedidos" 
+ *    summary: "Listado de los Estados de los Pedidos"
+ *    description: Devuelve todos los Estaados de Pedidos disponibles
+ *    parameters:
+ *    - name: authorization
+ *      description: token de autorizacion para acceder a la operacion 
+ *      in: header
+ *      required: false
+ *      type: string
+ *      example: bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuaWNrbmFtZSI6ImRhdmVHIiwicGFzc3dvcmQiOiIxNDZiZWE5MjdhNjc0M2MwMjZmNDA4NGIwNjFkM2MxYyIsImlkX3VzZXJfc3RhdGUiOjEsImlhdCI6MTYzNjA3OTA4MCwiZXhwIjoxNjM2MDgyNjgwfQ.s-y0FRh4ebdMAhgAsb7mW7Bt1UQ1UZ09z0-t9QYpYPA
+ *    responses:
+ *      200:
+ *        description: Success
+ *      401:
+ *        description: Unauthorized
+ *      404:
+ *        description: Not found
+ */
+
+/**
+ * @swagger
+ * /order-states:
+ *  post:
+ *    tags:
+ *    - "Estados de los Pedidos"
+ *    summary: "Agrega Estado de Pedido"
+ *    description: Guarda un nuevo estado de un pedido en nuestra app
+ *    parameters:
+ *    - name: authorization
+ *      description: token de autorizacion para acceder a la operacion 
+ *      in: header
+ *      required: false
+ *      type: string
+ *      example: bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuaWNrbmFtZSI6ImRhdmVHIiwicGFzc3dvcmQiOiIxNDZiZWE5MjdhNjc0M2MwMjZmNDA4NGIwNjFkM2MxYyIsImlkX3VzZXJfc3RhdGUiOjEsImlhdCI6MTYzNjA3OTA4MCwiZXhwIjoxNjM2MDgyNjgwfQ.s-y0FRh4ebdMAhgAsb7mW7Bt1UQ1UZ09z0-t9QYpYPA
+ *    - name: name
+ *      description: Nombre del producto
+ *      in: formData
+ *      required: true
+ *      type: string
+ *    responses:
+ *      200:
+ *        description: Success
+ *      401:
+ *        description: Unauthorized
+ *      404:
+ *        description: Not found
+ */
+
+/**
+ * @swagger
+ * /order-states/{id}:
+ *  get:
+ *    tags:
+ *    - "Estados de los Pedidos"
+ *    summary: "Ver info por ID"
+ *    description: Todos los datos de un estado de pedido
+ *    parameters:
+ *    - name: authorization
+ *      description: token de autorizacion para acceder a la operacion 
+ *      in: header
+ *      required: false
+ *      type: string
+ *      example: bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuaWNrbmFtZSI6ImRhdmVHIiwicGFzc3dvcmQiOiIxNDZiZWE5MjdhNjc0M2MwMjZmNDA4NGIwNjFkM2MxYyIsImlkX3VzZXJfc3RhdGUiOjEsImlhdCI6MTYzNjA3OTA4MCwiZXhwIjoxNjM2MDgyNjgwfQ.s-y0FRh4ebdMAhgAsb7mW7Bt1UQ1UZ09z0-t9QYpYPA
+ *    - name: id
+ *      description: Id de estado del Pedido
+ *      in: path
+ *      required: true
+ *      type: integer
+ *    responses:
+ *      200:
+ *        description: Success
+ *      404:
+ *        description: Not found
+ */
+
+/**
+ * @swagger
+ * /order-states/{id}:
+ *  put:
+ *    tags:
+ *    - "Estados de los Pedidos"
+ *    summary: "Modifica por ID"
+ *    description: "Se realiza la modificación en uno o más campos de un Estado de Pedido"
+ *    parameters:
+ *    - name: authorization
+ *      description: token de autorizacion para acceder a la operacion 
+ *      in: header
+ *      required: false
+ *      type: string
+ *      example: bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuaWNrbmFtZSI6ImRhdmVHIiwicGFzc3dvcmQiOiIxNDZiZWE5MjdhNjc0M2MwMjZmNDA4NGIwNjFkM2MxYyIsImlkX3VzZXJfc3RhdGUiOjEsImlhdCI6MTYzNjA3OTA4MCwiZXhwIjoxNjM2MDgyNjgwfQ.s-y0FRh4ebdMAhgAsb7mW7Bt1UQ1UZ09z0-t9QYpYPA
+ *    - name: id
+ *      description: Id de Estado de Pedido
+ *      in: path
+ *      required: true
+ *      type: integer
+ *    - name: name
+ *      description: Nombre del estado del pedido
+ *      in: formData
+ *      required: false
+ *      type: string
+ *    responses:
+ *      200:
+ *        description: Success
+ *      401:
+ *        description: Unauthorized
+ *      404:
+ *        description: Not found
+ */
+
+/**
+ * @swagger
+ * /order-states/{id}:
+ *  delete:
+ *    tags:
+ *    - "Estados de los Pedidos"
+ *    summary: "Elimina por ID"
+ *    description: "Se elimina un estado del pedido en nuestra base de datos"
+ *    parameters:
+ *    - name: authorization
+ *      description: token de autorizacion para acceder a la operacion 
+ *      in: header
+ *      required: false
+ *      type: string
+ *      example: bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuaWNrbmFtZSI6ImRhdmVHIiwicGFzc3dvcmQiOiIxNDZiZWE5MjdhNjc0M2MwMjZmNDA4NGIwNjFkM2MxYyIsImlkX3VzZXJfc3RhdGUiOjEsImlhdCI6MTYzNjA3OTA4MCwiZXhwIjoxNjM2MDgyNjgwfQ.s-y0FRh4ebdMAhgAsb7mW7Bt1UQ1UZ09z0-t9QYpYPA
+ *    - name: id
+ *      description: Id de estado del Pedido
+ *      in: path
+ *      required: true
+ *      type: integer
+ *    responses:
+ *      200:
+ *        description: Success
+ *      401:
+ *        description: Unauthorized
+ *      404:
+ *        description: Not found
+ */
+
 module.exports = router;
