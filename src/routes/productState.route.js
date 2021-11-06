23db1a8f1195231e6ba9467e79885ec50b409d2a
Javier Alejandro Oyarzo Carcamo <javi_14_228@hotmail.com>
2021-11-06 18:28:58 -0300

    ADD CRRD operations in Swagger Product States

--- src/routes/productState.route.js
+++ src/routes/productState.route.js
@@ -91,4 +91,157 @@ router.get("/:id", all.isAdmin, (req, res) => {
   });
 });
 
+/**
+ * @swagger
+ * /product-states:
+ *  get:
+ *    tags:
+ *    - "Estado de los Productos" 
+ *    summary: "Listado de todos Estado de los Productos"
+ *    description: Devuelve todos los Estado de los Productos disponibles
+ *    parameters:
+ *    - name: authorization
+ *      description: token de autorizacion para acceder a la operacion 
+ *      in: header
+ *      required: false
+ *      type: string
+ *      example: bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuaWNrbmFtZSI6ImRhdmVHIiwicGFzc3dvcmQiOiIxNDZiZWE5MjdhNjc0M2MwMjZmNDA4NGIwNjFkM2MxYyIsImlkX3VzZXJfc3RhdGUiOjEsImlhdCI6MTYzNjA3OTA4MCwiZXhwIjoxNjM2MDgyNjgwfQ.s-y0FRh4ebdMAhgAsb7mW7Bt1UQ1UZ09z0-t9QYpYPA
+ *    responses:
+ *      200:
+ *        description: Success
+ *      401:
+ *        description: Unauthorized
+ *      404:
+ *        description: Not found
+ */
+
+/**
+ * @swagger
+ * /product-states/{id}:
+ *  get:
+ *    tags:
+ *    - "Estado de los Productos"
+ *    summary: "Ver info por ID"
+ *    description: Todos los datos de un Estado de los Productos
+ *    parameters:
+ *    - name: authorization
+ *      description: token de autorizacion para acceder a la operacion 
+ *      in: header
+ *      required: false
+ *      type: string
+ *      example: bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuaWNrbmFtZSI6ImRhdmVHIiwicGFzc3dvcmQiOiIxNDZiZWE5MjdhNjc0M2MwMjZmNDA4NGIwNjFkM2MxYyIsImlkX3VzZXJfc3RhdGUiOjEsImlhdCI6MTYzNjA3OTA4MCwiZXhwIjoxNjM2MDgyNjgwfQ.s-y0FRh4ebdMAhgAsb7mW7Bt1UQ1UZ09z0-t9QYpYPA
+ *    - name: id
+ *      description: Id de Estado de los Productos
+ *      in: path
+ *      required: true
+ *      type: integer
+ *    responses:
+ *      200:
+ *        description: Success
+ *      404:
+ *        description: Not found
+ */
+
+/**
+ * @swagger
+ * /product-states:
+ *  post:
+ *    tags:
+ *    - "Estado de los Productos"
+ *    summary: "Agrega Estado de los Productos"
+ *    description: Guarda un nuevo Estado de los Productos en nuestra app
+ *    parameters:
+ *    - name: authorization
+ *      description: token de autorizacion para acceder a la operacion 
+ *      in: header
+ *      required: false
+ *      type: string
+ *      example: bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuaWNrbmFtZSI6ImRhdmVHIiwicGFzc3dvcmQiOiIxNDZiZWE5MjdhNjc0M2MwMjZmNDA4NGIwNjFkM2MxYyIsImlkX3VzZXJfc3RhdGUiOjEsImlhdCI6MTYzNjA3OTA4MCwiZXhwIjoxNjM2MDgyNjgwfQ.s-y0FRh4ebdMAhgAsb7mW7Bt1UQ1UZ09z0-t9QYpYPA
+ *    - name: name
+ *      description: Nombre del Estado de los Productos
+ *      in: formData
+ *      required: true
+ *      type: string
+ *    responses:
+ *      200:
+ *        description: Success
+ *      401:
+ *        description: Unauthorized
+ *      404:
+ *        description: Not found
+ */
+
+/**
+ * @swagger
+ * /product-states/{id}:
+ *  put:
+ *    tags:
+ *    - "Estado de los Productos"
+ *    summary: "Modifica por ID"
+ *    description: "Se realiza la modificación en uno o más campos de un Estado de los Productos"
+ *    parameters:
+ *    - name: authorization
+ *      description: token de autorizacion para acceder a la operacion 
+ *      in: header
+ *      required: false
+ *      type: string
+ *      example: bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuaWNrbmFtZSI6ImRhdmVHIiwicGFzc3dvcmQiOiIxNDZiZWE5MjdhNjc0M2MwMjZmNDA4NGIwNjFkM2MxYyIsImlkX3VzZXJfc3RhdGUiOjEsImlhdCI6MTYzNjA3OTA4MCwiZXhwIjoxNjM2MDgyNjgwfQ.s-y0FRh4ebdMAhgAsb7mW7Bt1UQ1UZ09z0-t9QYpYPA
+ *    - name: id
+ *      description: Id de producto
+ *      in: path
+ *      required: true
+ *      type: integer
+ *    - name: name
+ *      description: Nombre del Estado de los Productos
+ *      in: formData
+ *      required: false
+ *      type: string
+ *    - name: price
+ *      description: Precio del producto
+ *      in: formData
+ *      required: false
+ *      type: number
+ *    - name: pic
+ *      description: Imagen de referencia
+ *      in: formData
+ *      required: false
+ *      type: string
+ *    responses:
+ *      200:
+ *        description: Success
+ *      401:
+ *        description: Unauthorized
+ *      404:
+ *        description: Not found
+ */
+
+/**
+ * @swagger
+ * /product-states/{id}:
+ *  delete:
+ *    tags:
+ *    - "Estado de los Productos"
+ *    summary: "Elimina por ID"
+ *    description: "Se elimina un Estado de los Productos en nuestra base de datos"
+ *    parameters:
+ *    - name: authorization
+ *      description: token de autorizacion para acceder a la operacion 
+ *      in: header
+ *      required: false
+ *      type: string
+ *      example: bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuaWNrbmFtZSI6ImRhdmVHIiwicGFzc3dvcmQiOiIxNDZiZWE5MjdhNjc0M2MwMjZmNDA4NGIwNjFkM2MxYyIsImlkX3VzZXJfc3RhdGUiOjEsImlhdCI6MTYzNjA3OTA4MCwiZXhwIjoxNjM2MDgyNjgwfQ.s-y0FRh4ebdMAhgAsb7mW7Bt1UQ1UZ09z0-t9QYpYPA
+ *    - name: id
+ *      description: Id de Estado de los Productos
+ *      in: path
+ *      required: true
+ *      type: integer
+ *    responses:
+ *      200:
+ *        description: Success
+ *      401:
+ *        description: Unauthorized
+ *      404:
+ *        description: Not found
+ */
+
 module.exports = router;
