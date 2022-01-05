54537cf8adb60e6e6f99d570c6ba4f306f0469f0
Ubuntu <ubuntu@ip-172-31-4-172.sa-east-1.compute.internal>
2022-01-05 03:45:35 +0000

    Conecction RDS OK

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const app = require('./server');
 const connection = require("./config/db.config");
-const PORT = process.env.PORT || 3030;
+const PORT = process.env.PORT || 3000;
 const host = 'http://localhost'
 const URL = `${host}:${PORT}`;
 
