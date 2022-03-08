43ab321ab8d85bcd503a82ef916ac877f070a534
Javier Alejandro Oyarzo <javi_14_228@hotmail.com>
2022-03-08 16:52:20 -0300

    Fix body fetch

--- src/public/script.js
+++ src/public/script.js
@@ -19,7 +19,7 @@ formElement.addEventListener("submit", (event) => {
 
     fetch('https://www.delilah-resto.ga/api/login', {
         method : 'POST',
-        body   : transaction
+        body   : transactionJson
     })
     .then(response => response.json())
     .then(data => console.log(data));
