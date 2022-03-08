7f4be5995099798c5fbc1c02815406a4f31c61b9
Javier Alejandro Oyarzo <javi_14_228@hotmail.com>
2022-03-08 12:36:22 -0300

    add script js on frontend

---
+++ src/public/script.js
@@ -0,0 +1,24 @@
+const formElement = document.getElementById("saveLogin");
+
+formElement.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    let user = document.getElementById('user').value;
+    let password = document.getElementById('password').value;
+
+    let transaction = {
+        user: user,
+        password: password
+    };
+
+    let transactionJson = JSON.stringify(transaction);
+
+    // MANDAR LOS DATOS DEL BACKEND Y GUARDARLOS AHI
+    console.log(transaction);
+    console.log(transactionJson);
+
+    fetch('https://www.delilah-resto.ga/login', {
+        method : 'POST',
+        body   : transactionJson
+    })
+})
