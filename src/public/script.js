cfefc364f352d7037d6f5b981e06babcae95764c
Javier Alejandro Oyarzo <javi_14_228@hotmail.com>
2022-03-09 10:17:13 -0300

    add content type

--- src/public/script.js
+++ src/public/script.js
@@ -1,4 +1,29 @@
+const passport = require('passport')
+  , LocalStrategy = require('passport-local').Strategy;
+const FacebookStrategy = require('passport-facebook').Strategy;
 const formElement = document.getElementById("saveLogin");
+
+passport.use(new FacebookStrategy({
+    clientID: secret_config.federation.facebook.client_id,
+    clientSecret: secret_config.federation.facebook.secret_id,
+    callbackURL: secret_config.federation.facebook.callback_url,
+    profileFields: ['id', 'email', 'gender', 'link', 'locale', 'name', 'timezone',
+      'updated_time', 'verified', 'displayName']
+  }, function (accessToken, refreshToken, profile, done) {
+    const _profile = profile._json;
+    
+    console.log('Facebook login info');
+    console.info(_profile);
+    
+    loginByThirdparty({
+      'auth_type': 'facebook',
+      'auth_id': _profile.id,
+      'auth_name': _profile.name,
+      'auth_email': _profile.id
+    }, done);
+  }
+));
+// ---------------------------  HASTA ACA COPIADO Y FALTA LA PRUEBA
 
 formElement.addEventListener("submit", (event) => {
     event.preventDefault();
@@ -22,7 +47,10 @@ formElement.addEventListener("submit", (event) => {
 
     fetch('https://www.delilah-resto.ga/api/login', {
         method : 'POST',
-        body   : transactionJson
+        body   : transactionJson,
+        headers : {
+          'Content-Type' : 'application/json'
+        }
     })
     .then(response => response.json())
     .then(data => console.log(data))
